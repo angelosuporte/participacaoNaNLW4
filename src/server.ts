6f3ff18d7c1d94a543ee122973e2a2a5c741e440
Angelo Carvalho <angelosuporteti@gmail.com>
2021-02-22 15:44:40 -0300

    Using method GET with response.json

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 
+
+app.get("/users", (request, response)=>{
+    return response.json({message: "Hello World - NLW04"});
+});
 
 app.listen(3333, () => console.log("server is running!"));
