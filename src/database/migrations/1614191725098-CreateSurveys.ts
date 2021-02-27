5c2dbcfd2494d29c5e7bd792fd510a8a6755deb2
Angelo Carvalho <angelosuporteti@gmail.com>
2021-02-26 22:18:12 -0300

    Created migration of survey

--- src/database/migrations/1614191725098-CreateSurveys.ts
+++ src/database/migrations/1614191725098-CreateSurveys.ts
@@ -1,11 +1,11 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export class CreateUser1614104372023 implements MigrationInterface {
+export class CreateSurveys1614191725098 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: "users",
+                name: "surveys",
                 columns: [
                     {
                         name: "id",
@@ -13,11 +13,11 @@ export class CreateUser1614104372023 implements MigrationInterface {
                         isPrimary: true,
                     },
                     {
-                        name: "name",
+                        name: "title",
                         type: "varchar",
                     },
                     {
-                        name: "email",
+                        name: "description",
                         type: "varchar",
                     },
                     {
@@ -31,6 +31,6 @@ export class CreateUser1614104372023 implements MigrationInterface {
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable("users");
+        await queryRunner.dropTable("surveys");
     }
 }
